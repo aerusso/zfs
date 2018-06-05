bb893ba6867ed8a18f1fd5ca5a58da1693e2c7e5
Antonio Russo <antonio.e.russo@gmail.com>
2018-06-05 19:19:45 -0400

    Use Linux-Maximum META to specify compatibility
    Signed-off-by: Antonio Russo <antonio.e.russo@gmail.com>

--- debian/get_next.sh
+++ debian/get_next.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SUPPORT=$(cat debian/linux_compat)
+SUPPORT=$(grep '^Linux-Maximum:' META | cut -d ':' -f 2 | tr -d ' \n' )
 MAJOR=$(echo $SUPPORT|cut -d'.' -f1)
 MINOR=$(echo $SUPPORT|cut -d'.' -f2)
 NEXT=$((MINOR+1))
